c717b9ed9f8bbe5c74374f776e8ab07b100488a4
David Dawson <me@daviddawson.me>
2017-08-02 11:32:55 +0100

    Mongoautoconfig not automatically loaded when mongo not in use.

--- src/main/groovy/io/muoncore/photonlite/EventStoreApplication.groovy
+++ src/main/groovy/io/muoncore/photonlite/EventStoreApplication.groovy
@@ -16,13 +16,14 @@ import org.apache.commons.cli.Options
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication(exclude = [
         HibernateJpaAutoConfiguration,
-        DataSourceAutoConfiguration
+        DataSourceAutoConfiguration, MongoAutoConfiguration
 ])
 @Configuration
 @Slf4j
